ed2821659869d39879ca86778dcff80d741185a8
Sonam Rinzin Gurung <sonamrinzingurung@gmail.com>
2025-04-29 11:06:03 -0500

    routing fixes

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,12 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="portfolio" element={<Layout />}>
         <Route index element={<Homepage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="blogs" element={<Blogs />} />
+        <Route path="contact" element={<Contact />} />
       </Route>
     </Routes>
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,29 +3,29 @@ import { NavLink } from "react-router"
 const NavBar = () => {
     return (
         <div className='flex gap-2 md:gap-8 ml-auto my-6 flex-wrap'>
-            <NavLink to={"/"}
+            <NavLink to={""}
                 className={({ isActive }) => `
                  ${isActive ? "text-teal-500" : ""}
                 font-semibold
                  `}
             >Homepage</NavLink>
-            <NavLink to={"/about"}
+            <NavLink to={"about"}
                 className={({ isActive }) => `
                  ${isActive ? "text-teal-500" : ""}
                 font-semibold
                  `}
             >About</NavLink>
-            <NavLink to={"/projects"}
+            <NavLink to={"projects"}
                 className={({ isActive }) => `
                  ${isActive ? "text-teal-500" : ""}
                 font-semibold`}
             >Projects</NavLink>
-            <NavLink to={"/blogs"}
+            <NavLink to={"blogs"}
                 className={({ isActive }) => `
                  ${isActive ? "text-teal-500" : ""}
                 font-semibold`}
             >Blogs</NavLink>
-            <NavLink to={"/contact"}
+            <NavLink to={"contact"}
                 className={({ isActive }) => `
                  ${isActive ? "text-teal-500" : ""}
                font-semibold `}
